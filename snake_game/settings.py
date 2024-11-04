4fae724fc1ddc91ddce5b05a10961d092e24512f
Thiện Nhân <nhanntn2203@gmail.com>
2024-11-04 21:02:51 +0700

    Templates changing

--- snake_game/settings.py
+++ snake_game/settings.py
@@ -53,9 +53,7 @@ MIDDLEWARE = [
 ROOT_URLCONF = 'snake_game.urls'
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-]
+STATICFILES_DIRS = [BASE_DIR / 'static']
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 TEMPLATES = [
